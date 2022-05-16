9463b74ca3844527856a4ec61973cf3595fbb65b
Alessandro Volpicella <sandro@hashnode.com>
2022-05-16 07:22:24 +0200

    Remove type from request

--- pages/api/consumer/success.ts
+++ pages/api/consumer/success.ts
@@ -1,10 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<void>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("Get success request");
-  res.status(200).json();
-  return;
+  return res.status(200).json({ data: "success" });
+  
 }
